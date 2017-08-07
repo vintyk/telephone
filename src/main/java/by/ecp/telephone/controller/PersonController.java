975454f558850a6325d4f9b7436d8b78f3bc79ea
Vintyk <vinty1978@gmail.com>
2017-08-07 15:07:31 +0300

    На английском поиск работает.

--- src/main/java/by/ecp/telephone/controller/PersonController.java
+++ src/main/java/by/ecp/telephone/controller/PersonController.java
@@ -6,11 +6,13 @@ import by.ecp.telephone.service.interfaces.PersonService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
-import java.util.Objects;
 import java.util.Optional;
 
 @Controller
@@ -32,7 +34,7 @@ public class PersonController {
     public ModelAndView showPersonsPage(
             HttpSession httpSession,
             @RequestParam("pageSize") Optional<Integer> pageSize,
-                                        @RequestParam("page") Optional<Integer> page) {
+            @RequestParam("page") Optional<Integer> page) {
         ModelAndView modelAndView = new ModelAndView("persons");
         int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
         int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
@@ -59,9 +61,9 @@ public class PersonController {
         int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
         int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
 
-        if (searchResult.equals("${litera}")){
-            searchResult=searchR;
-            if (searchResult.equals("")){
+        if (searchResult.equals("${litera}")) {
+            searchResult = searchR;
+            if (searchResult.equals("")) {
                 Page<Person> persons = personService.findAllPageableOrderBylastName(new PageRequest(evalPage, evalPageSize));
                 Pager pager = new Pager(persons.getTotalPages(), persons.getNumber(), BUTTONS_TO_SHOW);
                 Person person = new Person();
@@ -73,13 +75,21 @@ public class PersonController {
                 modelAndView.addObject("pager", pager);
                 return modelAndView;
             }
-
         }
-        searchR=searchResult;
-        if (searchR.length()>0) {
+        searchR = searchResult;
+        if (searchR.length() > 0) {
             httpSession.setAttribute("litera", searchR);
         }
-
+        if (searchR.length() > 1) {
+            Page<Person> persons = personService.findAllByLastNameContainsOrderByLastName(new PageRequest(evalPage, evalPageSize), searchResult);
+            Pager pager = new Pager(persons.getTotalPages(), persons.getNumber(), BUTTONS_TO_SHOW);
+            Person person = new Person();
+            modelAndView.addObject("persons", persons);
+            modelAndView.addObject("selectedPageSize", evalPageSize);
+            modelAndView.addObject("pageSizes", PAGE_SIZES);
+            modelAndView.addObject("pager", pager);
+            return modelAndView;
+        }
         Page<Person> persons = personService.findAllByAlphabetEqualsOrderByLastName(new PageRequest(evalPage, evalPageSize), searchResult);
         Pager pager = new Pager(persons.getTotalPages(), persons.getNumber(), BUTTONS_TO_SHOW);
         Person person = new Person();
@@ -91,22 +101,14 @@ public class PersonController {
         modelAndView.addObject("pager", pager);
         return modelAndView;
     }
-    @GetMapping("/search/{searchRes}")
-    public ModelAndView showPersonsPageSearch(@PathVariable String searchRes,
-                                              @RequestParam("pageSize") Optional<Integer> pageSize,
-                                              @RequestParam("page") Optional<Integer> page) {
-        ModelAndView modelAndView = new ModelAndView("persons");
-        int evalPageSize = pageSize.orElse(INITIAL_PAGE_SIZE);
-        int evalPage = (page.orElse(0) < 1) ? INITIAL_PAGE : page.get() - 1;
 
-        Page<Person> persons = personService.findAllByLastNameContainsOrderByLastName(new PageRequest(evalPage, evalPageSize), searchRes);
-        Pager pager = new Pager(persons.getTotalPages(), persons.getNumber(), BUTTONS_TO_SHOW);
-        Person person = new Person();
-        modelAndView.addObject("persons", persons);
-        modelAndView.addObject("selectedPageSize", evalPageSize);
-        modelAndView.addObject("pageSizes", PAGE_SIZES);
-        modelAndView.addObject("pager", pager);
-        return modelAndView;
+    //    @RequestMapping(value = "/", method = RequestMethod.POST)
+//    public String reciveSearchResult(@RequestParam String searchBody, ModelMap model) {
+//        model.addAttribute(SEARCH_BODY, searchBody);
+//        return modelAndView;
+//    }
+    @PostMapping(path = "/")
+    public String search(@RequestParam String litera){
+        return "redirect:"+litera;
     }
-
 }
