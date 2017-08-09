80db9bc9421e27336a2651240296cc10209d0a98
Vintyk <vinty1978@gmail.com>
2017-08-09 10:28:12 +0300

    Кракозябры при POST запросе на SAVE (UPDATE)объекта.

--- src/main/java/by/ecp/telephone/config/SecurityConfig.java
+++ src/main/java/by/ecp/telephone/config/SecurityConfig.java
@@ -35,10 +35,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-//        filter.setEncoding("UTF-8");
-//        filter.setForceEncoding(true);
-//        http.addFilterBefore(filter, CsrfFilter.class);
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        filter.setForceEncoding(true);
+        http.addFilterBefore(filter, CsrfFilter.class);
         http
                 .csrf().disable()
                 .authorizeRequests()
